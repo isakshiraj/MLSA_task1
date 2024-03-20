6c832a2918f4d70870d8f64fd96e3cc9ff92fb8e
Sakshi Raj <118617849+isakshiraj@users.noreply.github.com>
2024-03-20 08:09:41 +0000

    aboutcommit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/tech.jpg";
+import image from "https://pixabay.com/vectors/robot-machine-technology-science-312566/";
 
 const imageAltText = "tech";
 
