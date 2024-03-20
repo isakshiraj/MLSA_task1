fef2c33430f7ae61837d1d88b3cfa732d198f0c7
Sakshi Raj <118617849+isakshiraj@users.noreply.github.com>
2024-03-20 06:30:21 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sakshi Raj",
+  title: "Student abassador",
+  email: "sakshi.raj1@studentambassadors.com",
+  gitHub: "https://github.com/isakshiraj",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/sakshi-raj-140a20249/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
